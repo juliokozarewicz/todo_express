cdcfeff310e0e4f7cc15af67162ad099263e730b
Julio Kozarewicz <julioerk@outlook.com>
2024-09-26 14:48:01 -0300

    [DATABASE] Databse config init.

--- tasks/entitys/entitys.ts
+++ tasks/entitys/entitys.ts
@@ -1,4 +1,28 @@
-// Placeholder for future entity definitions
-export interface HelloWorldEntity {
-    message: string;
-}
+import {
+    Column, CreateDateColumn, Entity, PrimaryGeneratedColumn,
+    UpdateDateColumn
+  } from 'typeorm'
+  
+  @Entity()
+  export class TasksEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+  
+    @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+  
+    @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    updatedAt: Date;
+  
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    taskName: string;
+  
+    @Column({ type: 'varchar', length: 500, nullable: true })
+    description: string;
+  
+    @CreateDateColumn({ nullable: false })
+    dueDate: Date;
+  
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    statusName: string;
+  }
--- tasks/server.ts
+++ tasks/server.ts
@@ -2,11 +2,30 @@ import express from 'express';
 import routes from './routes';
 import { config } from 'dotenv';
 import path from 'path';
+import "reflect-metadata"
+import { DataSource, DataSourceOptions } from "typeorm"
+import { TasksEntity } from './entitys/entitys';
 
 // load '.env'
 //----------------------------------------------------------------------------
 config({ path: path.resolve(__dirname, '../.env') });
 //----------------------------------------------------------------------------
+
+// database
+//------------------------------------------------------------------------
+const AppDataSource = new DataSource({
+    type: process.env.DATABASE_TYPE,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    synchronize: process.env.DATABASE_SYNC,
+    entities: [TasksEntity],
+  } as DataSourceOptions )
+  
+  AppDataSource.initialize()
+  //------------------------------------------------------------------------
 
 // express server
 //----------------------------------------------------------------------------
