023b42c6b08d46361118ac065739cde2e7d0a727
Julio Kozarewicz <julioerk@outlook.com>
2024-09-30 12:10:00 -0300

    [RATELIMITER] Rate limiter finished.

--- tasks/logsGenerator.ts
+++ tasks/logsGenerator.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 const logsDir = path.resolve('./1_logs/');
-const logFilePath = path.resolve('./1_logs/logs.txt');
+const logFilePath = path.resolve('./1_logs/main.log');
 
 interface LogEntry {
     ip: string;
