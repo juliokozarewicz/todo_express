6dcd7db4003aa61271398531b1d50d621a82e202
Julio Kozarewicz <julioerk@outlook.com>
2024-09-30 12:01:38 -0300

    [RATELIMITER] Rate limiter finished.

--- tasks/middlewares/rateLimiter.ts
+++ tasks/middlewares/rateLimiter.ts
@@ -3,12 +3,12 @@ import rateLimit from 'express-rate-limit';
 // rate limiter
 //------------------------------------------------------------------------
 export const rateLimiter = rateLimit({
-    windowMs: 10 * 60 * 1000,
-    max: 100, 
-    message: {
-      "status": "error",
-      "code": 429,
-      "message": 'Too many requests, please try again later',
-    }
-  });
-  //------------------------------------------------------------------------
+  windowMs: 10 * 60 * 1000,
+  max: 100, 
+  message: {
+    "status": "error",
+    "code": 429,
+    "message": 'Too many requests, please try again later',
+  }
+});
+//------------------------------------------------------------------------
